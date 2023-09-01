6829ea28b46ef9c22a019bfdcec747e73acc3780
Md Sulaiman <51925710+sulaiman-coder@users.noreply.github.com>
2023-09-02 02:33:29 +0600

    Create compress_videos.sh

---
+++ compress_videos.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+BASE_PATH="src/DomainManager/src/assets/videos"
+
+for f in "$BASE_PATH"/*.mp4; do
+  ffmpeg -i "$f" "$BASE_PATH/output.mp4" -y
+  cp "$BASE_PATH/output.mp4" "$f"
+done
+
+rm "$BASE_PATH/output.mp4"
